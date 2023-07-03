8c7cdd053914c28cf01f969977f3c1b1faa11d78
xpc <korotinandrey@mail.ru>
2023-07-03 20:45:15 +0400

    Lesson

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Коротин", "Андрей", (byte) 31);
+        userService.saveUser("Афанасьев", "Олег", (byte) 28);
+        userService.saveUser("Плюшкина", "Анна", (byte) 30);
+        userService.saveUser("Джордж", "Мартин", (byte) 74);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
